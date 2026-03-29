0ace1995042bab4b67fee5b67a1d47cda8779a9c
haddadadnane4-glitch <haddadadnane4@gmail.com>
2026-03-29 15:40:14 +0100

    Create web/index.js

---
+++ web/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Minecraft server is running!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Running on port " + port);
+});
