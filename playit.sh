c109b16531c3001eafe21ee2e49d38368021f1f2
haddadadnane4-glitch <haddadadnane4@gmail.com>
2026-03-29 16:11:30 +0100

    Update playit.sh

--- playit.sh
+++ playit.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo "Starting Minecraft server..."
-
-java -Xmx1G -Xms1G -jar server/paper.jar nogui &
-
 echo "Starting Playit tunnel..."
 
-./playit --secret $PLAYIT_SECRET
-
-echo "Starting web server..."
-
-node web/index.js
+./playit --secret "$PLAYIT_SECRET"
