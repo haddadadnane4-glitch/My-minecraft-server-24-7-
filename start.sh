da8cd7d382502959cfb25d42326313d8d748fb2f
haddadadnane4-glitch <haddadadnane4@gmail.com>
2026-03-29 22:09:12 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
 mkdir -p /data/plugins
-
-echo "Downloading plugins..."
-
-cd /data/plugins
-
-curl -L -o ViaVersion.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/latest
-curl -L -o ViaBackwards.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaBackwards/versions/latest
-curl -L -o ViaRewind.jar https://github.com/ViaVersion/ViaRewind/releases/latest/download/ViaRewind.jar
-curl -L -o Geyser-Spigot.jar https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot
-
 cd /data
-
 java -jar paper*.jar
